547e88c3a3847ed635f6bc62d18456bea8ffe0a0
akash <akash329@protonmail.com>
2024-02-13 11:21:21 +0530

    user profile editable now2

--- lib/features/user_profile/repository/user_profile_repository.dart
+++ lib/features/user_profile/repository/user_profile_repository.dart
@@ -1,5 +1,15 @@
 import 'package:ceddit/core/constants/firebase_constants.dart';
+import 'package:ceddit/core/failure.dart';
+import 'package:ceddit/core/providers/firebase_providers.dart';
+import 'package:ceddit/core/type_defs.dart';
+import 'package:ceddit/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+
+final userProfileRepositoryProvider = Provider((ref) {
+  return UserProfileRepository(firestore: ref.watch(firestoreProvider));
+});
 
 class UserProfileRepository {
   final FirebaseFirestore _firestore;
@@ -8,4 +18,14 @@ class UserProfileRepository {
 
   CollectionReference get _users =>
       _firestore.collection(FirebaseConstants.usersCollection);
+
+  FutureVoid editProfile(UserModel user) async {
+    try {
+      return right(_users.doc(user.uid).update(user.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
 }
